fb5332766362deae22a817126f0d79461b563d30
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-05-31 21:56:25 -0300

    feat (leitores.js): add crud - método criar

--- src/leitores.js
+++ src/leitores.js
@@ -1,6 +1,7 @@
 const readline = require('readline/promises');
 const crypto = require('crypto');
 const Leitor = require('./classes/Leitor');
+const LeitorCrud = require('./classes/LeitorCrud');
 
 const input = readline.createInterface({
     input: process.stdin,
@@ -19,14 +20,21 @@ async function run() {
     switch (resposta) {
         case 'criar':
             const leitor = new Leitor();
-            leitor.codigo = generateRandomCode();
-            leitor.nome = await input.question('Digite o nome do leitor: ');
-            leitor.cpf = await input.question('Digite o CPF do leitor: ');
-            leitor.dataNascimento = await input.question('Digite a data de nascimento do leitor: ');
+            const codigo = generateRandomCode();
+            const nome = await input.question('Digite o nome do leitor: ');
+            const cpf = await input.question('Digite o CPF do leitor: ');
+            const dataNascimento = await input.question('Digite a data de nascimento do leitor: ');
+
+            leitor.setCodigo = codigo;
+            leitor.setNome = nome;
+            leitor.setCpf = cpf;
+            leitor.setDataNascimento = dataNascimento;
+
+            const crud = new LeitorCrud();
+            crud.criar(leitor);
 
             console.log('\n')
             console.log('Leitor criado com sucesso!');
-            console.log(leitor);
             input.close();
             break;
 
