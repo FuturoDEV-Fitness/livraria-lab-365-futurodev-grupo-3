6aa4ddfb37a30dba64ca069b4f6b96b7e9be35d6
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-06-01 13:32:34 -0300

    resolução exercício 02

--- src/classes/LivroCrud.js
+++ src/classes/LivroCrud.js
@@ -1,9 +1,64 @@
+const fs = require("fs");
 class LivroCrud {
+  constructor() {
+    this.filePath = "./src/files/livros.json";
+  }
 
-    constructor() {
-        this.filePath = './src/files/livros.json';
+  async lerDados() {
+    try {
+      const dados = await fs.readFileSync(this.filePath, "utf-8");
+      return JSON.parse(dados);
+    } catch (error) {
+      console.error("Erro ao ler dados: ", error);
+      return [];
     }
+  }
 
+  async escreverDados(data) {
+    try {
+      await fs.writeFileSync(
+        this.filePath,
+        JSON.stringify(data, null, 2),
+        "utf-8"
+      );
+    } catch (error) {
+      console.error("Erro ao escrever dados: ", error);
+    }
+  }
+
+  async criar(livro) {
+    const conteudoAtual = await this.lerDados();
+
+    conteudoAtual.push({
+      nome: livro.nome,
+      codigo: livro.codigo,
+      genero: livro.genero,
+      autor: livro.autor,
+      paginas: livro.paginas,
+    });
+
+    this.escreverDados(conteudoAtual);
+  }
+
+  async deletar(codigo) {
+    let livros = await this.lerDados();
+    livros = livros.filter((livro) => livro.codigo !== codigo);
+    this.escreverDados(livros);
+  }
+
+  async alterar(codigo, novosDados) {
+    let livros = await this.lerDados();
+    const index = livros.findIndex((livro) => livro.codigo === codigo);
+    if (index !== -1) {
+      livros[index] = { ...livros[index], ...novosDados };
+      this.escreverDados(livros);
+    }
+  }
+
+  async consultar(nome) {
+    const livros = await this.lerDados();
+    return livros.find((livro) => livro.nome == nome);
+  }
 }
 
 module.exports = LivroCrud;
--- src/livros.js
+++ src/livros.js
@@ -1,34 +1,81 @@
-const readline = require('readline/promises');
+const readline = require("readline/promises");
+const Livro = require("./classes/Livro");
+const LivroCrud = require("./classes/LivroCrud");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 async function run() {
+  const resposta = await rl.question(
+    "Escolha uma ação (criar, deletar, alterar, consultar): "
+  );
+  const crud = new LivroCrud();
 
-    const resposta = await rl.question('Escolha uma ação (criar, deletar, alterar, consultar): ');
+  switch (resposta) {
+    case "criar":
+      const genero = await rl.question("Qual o gênero do livro?");
+      const nome = await rl.question("Qual o título do livro?");
+      const paginas = await rl.question(
+        "Qual a quantidade de páginas do livro?"
+      );
+      const autor = await rl.question("Qual o autor do livro?");
 
-    switch (resposta) {
-        case 'criar':
-            /* Coloque sua resposta aqui  */
-            rl.close();
-            break;
-        case 'deletar': {
-            /* Coloque sua resposta aqui */
-            rl.close();
-            break;
-        }
-        case 'consultar': {
-            /* Coloque sua resposta aqui */
-            rl.close();
-            break;
-        }
-        default:
-            console.log("Ação não reconhecida.");
-            rl.close();
+      const livro = new Livro(nome, paginas, genero, autor);
+      await crud.criar(livro);
+      rl.close();
+      break;
+
+    case "deletar": {
+      const codigo = await rl.question(
+        "Qual o código do livro que será deletado?"
+      );
+      await crud.deletar(codigo);
+      console.log(`Livro com o código ${codigo} foi deletado com sucesso`);
+      rl.close();
+      break;
     }
 
+    case "alterar":
+      const codigoAlterar = await rl.question(
+        "Qual o código do livro a ser alterado? "
+      );
+      const novosDados = {};
+      const alterarNome = await rl.question(
+        "Novo nome (deixe vazio para não alterar): "
+      );
+      if (alterarNome) novosDados.nome = alterarNome;
+      const alterarGenero = await rl.question(
+        "Novo gênero (deixe vazio para não alterar): "
+      );
+      if (alterarGenero) novosDados.genero = alterarGenero;
+      const alterarPaginas = await rl.question(
+        "Novo número de páginas (deixe vazio para não alterar): "
+      );
+      if (alterarPaginas) novosDados.paginas = alterarPaginas;
+      const alterarAutor = await rl.question(
+        "Novo autor (deixe vazio para não alterar): "
+      );
+      if (alterarAutor) novosDados.autor = alterarAutor;
+      crud.alterar(codigoAlterar, novosDados);
+      console.log("Livro alterado com sucesso!");
+      break;
+
+    case "consultar": {
+      const nome = await rl.question(
+        "Qual o título do livro que a sr consultado?"
+      );
+
+      const livroConsultado = await crud.consultar(nome);
+      console.log(livroConsultado ? livroConsultado : "Livro não encontrado.");
+      rl.close();
+      break;
+    }
+    default:
+      console.log("Ação não reconhecida.");
+      rl.close();
+  }
 }
 
 run();
