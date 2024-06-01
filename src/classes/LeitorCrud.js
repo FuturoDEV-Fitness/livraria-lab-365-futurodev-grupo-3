6e0e4beb4fdafd0f5202f2141fc6abf0e8818946
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-05-31 21:54:18 -0300

    feat (leitorCrud.js): add método criar

--- src/classes/LeitorCrud.js
+++ src/classes/LeitorCrud.js
@@ -1,7 +1,29 @@
+const fs = require('fs');
 class LeitorCrud {
 
     constructor() {
-       
+        this.filePath = './src/files/leitores.json';
+    }
+
+    criar(leitor) {
+        
+        // lê o conteúdo que já existe no JSON
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+
+        // adiciona o novo leitor ao JSON
+        conteudoAtual.push({
+            codigo: leitor.getCodigo,
+            nome: leitor.getNome,
+            cpf: leitor.getCpf,
+            dataNascimento: leitor.getDataNascimento
+        });
+
+        // escreve o conteúdo atualizado no JSON
+        fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoAtual, null, 2), // null, 2 é para organizar o JSON
+            'utf-8'
+        )
     }
 
 }
