10b92620719ea9bbd37661f83397d6d29061c42c
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-06-03 19:27:24 -0300

    feat: realização exercício auditório - Bianca Barcelos

--- src/auditorios.js
+++ src/auditorios.js
@@ -1,4 +1,6 @@
 const readline = require('readline/promises');
+const Auditorio = require('./classes/Auditorio');
+const AuditorioCrud = require('./classes/AuditorioCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -6,21 +8,58 @@ const rl = readline.createInterface({
 });
 
 async function run() {
-
     const resposta = await rl.question('Escolha uma ação (criar, deletar, alterar, consultar): ');
+    const crud = new AuditorioCrud();
 
     switch (resposta) {
         case 'criar':
             /* Coloque sua resposta aqui  */
+            const nome = await rl.question('Qual o nome do auditório? ');
+            const descricao = await rl.question('Adicione uma descrição para o auditório: ');
+            const lotacaoMaxima = await rl.question('Qual a capacidade máxima do auditório? ');
+            const auditorio = new Auditorio();
+            
+            auditorio.setNome = nome;
+            auditorio.setDescricao = descricao;
+            auditorio.setLotacaoMaxima = lotacaoMaxima;
+
+            await crud.createAuditoruim(auditorio)
+            console.log('Auditório cadastrado com sucesso!')
             rl.close();
             break;
         case 'deletar': {
             /* Coloque sua resposta aqui */
+            const codigo = await rl.question('Informe o código do auditório a ser removido do registro:');
+            
+            await crud.deleteAuditorium(codigo);
+
+            console.log('Auditório ', codigo ,' removido com sucesso!');
+            rl.close();
+            break;
+        }
+        case 'alterar':{
+            /* Coloque sua resposta aqui */
+            const newData = {};
+            const code = await rl.question('Qual o código do auditório a ser alterado? ');
+            const newName = await rl.question('Qual o novo nome do auditório (para não alterar pressione somente enter): ');
+            const newDescription = await rl.question('Qual a nova descrição do auditório (para não alterar pressione somente enter): ');
+            const newMAximumLotation = await rl.question('Qual o novo numero de lotação do auditório (para não alterar pressione somente enter): ');
+
+            if(newName) newData.nome = newName
+            if(newDescription) newData.descricao = newDescription
+            if(newMAximumLotation) newData.lotacaoMaxima = newMAximumLotation
+            
+            crud.updateAuditorium(code, newData)
+
+            console.log('Auditório alterado com sucesso!');            
             rl.close();
             break;
         }
         case 'consultar': {
             /* Coloque sua resposta aqui */
+            const name = await rl.question('Busca de auditório por nome: ');
+            crud.readAuditorium(name);
+            
             rl.close();
             break;
         }
--- src/classes/AuditorioCrud.js
+++ src/classes/AuditorioCrud.js
@@ -1,9 +1,79 @@
+const fs = require("fs");
+
 class AuditorioCrud {
     
     constructor() {
         this.filePath = './src/files/auditorios.json';
     }
 
+    readData() {
+        try {
+            const data = fs.readFileSync(this.filePath,'utf-8');
+            return JSON.parse(data)
+        } catch (error) {
+            console.error("Não foi possível ler os dados:", error);
+            return [];
+        }
+    }
+
+    async writeData(data) {
+        try {
+            await fs.writeFileSync(
+                this.filePath,
+                JSON.stringify(data,null,2),
+                'utf-8',
+            )
+            
+        } catch (error) {
+            console.error("Não foi possível escrever os dados, erro:", error);
+        }
+    }
+
+    async createAuditoruim(auditorium){
+        const currentData = await this.readData();
+
+        currentData.push({
+            codigo: auditorium.getCodigo,
+            nome: auditorium.getNome,
+            descricao: auditorium.getDescricao,
+            lotacaoMaxima: auditorium.getLotacaoMaxima
+        })
+
+        this.writeData(currentData);
+    }
+
+    async readAuditorium(name){
+        //*************** verificar o por que ao finalizar a busca nao retorna para o menu principal ***************
+        const auditoriums = await this.readData();
+        const auditoriumsFind = auditoriums.find((auditorio) => auditorio.nome === name)
+
+        if(auditoriumsFind){
+            console.log(auditoriumsFind)
+        }else{
+            console.log('Não foi possível encontrar um auditório com o nome passado, verifique a digitação e tente novamente')
+        }
+    }
+
+    async updateAuditorium(code, newData) {
+        let auditoriums = await this.readData();
+        const index = auditoriums.findIndex((auditorium) => auditorium.codigo === code);
+        
+        if(index !== -1){
+            auditoriums[index] = {... auditoriums[index], ... newData}
+            this.writeData(auditoriums);
+        }
+    }
+
+    async deleteAuditorium(code) {
+        let auditoriums = await this.readData();
+        auditoriums.map(auditorium =>{
+            if(auditorium.codigo === code){
+                console.log(auditorium)
+            }
+        })
+        auditoriums = auditoriums.filter((auditorium) => auditorium.codigo !== code);
+        this.writeData(auditoriums)
+    }
 }
 
 module.exports = AuditorioCrud;
